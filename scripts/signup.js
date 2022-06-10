384f1908ffc9e5eefd8baa29ad5a67e8139d433b
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-06-10 17:40:18 +0530

    Signup logic added

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,13 +1,20 @@
-import { getItem } from "../functions/localStorage.js";
+import { getItem, setItem } from "../functions/localStorage.js";
 import { signupForm } from "../functions/signupForm.js";
 
 const form = document.getElementById('form');
-const userData = getItem("userData");
+const userData = getItem("userData") || [];
 
-form.addEventListener('submit', () => {
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
     const user = signupForm(form);
+    console.log('user:', user)
 
     if(user.isFilled().status){
-
+        userData.push(user);
+        setItem('userData', userData);
+        alert("Account created successfully");
+        window.location.href = ".././index.html";
+    } else {
+        alert(user.isFilled().message);
     }
-})
+});
