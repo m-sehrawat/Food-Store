f8ac8a3ec4d49138510becb6bb3b8d866578f5ba
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-06-11 00:35:44 +0530

    footer link will open in a new tab feature added

--- components/footer.js
+++ components/footer.js
@@ -4,13 +4,13 @@ export const footer = () => {
             <p class="text-center">Made with ❤️ by Mohit Sehrawat</p>
             <div>
                 <button class="btn btn-primary">
-                    <a href="https://www.linkedin.com/in/m-sehrawat/">LinkedIn</a>
+                    <a href="https://www.linkedin.com/in/m-sehrawat/" target="_blank">LinkedIn</a>
                 </button>
                 <button class="btn btn-success">
-                    <a href="https://github.com/m-sehrawat">GitHub</a>
+                    <a href="https://github.com/m-sehrawat" target="_blank">GitHub</a>
                 </button>
                 <button class="btn btn-danger">
-                    <a href="https://mohit-portfolio.vercel.app/">Portfolio</a>
+                    <a href="https://mohit-portfolio.vercel.app/" target="_blank">Portfolio</a>
                 </button>
             </div>
         </div>`
